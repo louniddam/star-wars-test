6b45f8e3d2e6e1cbc1dea45f863ded405548613d
Niddam <louniddam@gmail.com>
2022-04-05 11:42:13 +0200

    Project Set-up: -architecture -cleaning useles files -add assets -router created -first render of home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Typography } from "@mui/material"
+import Home from "./views/pages/home"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="App">
+	  <Router>
+		<Routes>
+		  <Route exact path="/" element={<Home/>} />
+		</Routes>
+	  </Router>
+	</div>
   );
 }
 
---
+++ src/views/pages/home/index.js
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material"
+import React from "react"
+
+const Home  = () => {
+
+	return(
+		<Typography>
+			Home Page
+		</Typography>
+	)
+}
+
+export default Home
