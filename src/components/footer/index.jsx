23b6e967ac3a056999487f11c0a7e2b6bcbf04ba
Niddam <louniddam@gmail.com>
2022-04-05 17:21:30 +0200

    feat: Footer finished

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,11 +6,11 @@ import { ReactComponent as Facebook } from '../../assets/social-media/icon_faceb
 import { ReactComponent as Twitter } from '../../assets/social-media/icon_twitter.svg';
 import Instagram from "../../assets/social-media/icon_instagram.png"
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
 	contentContainer: {
 		backgroundImage: `url(${Vader})`,
 		backgroundSize: 'cover',
-		minHeight: 400,
+		minHeight: 430,
 		backgroundPosition: "center"
 	},
 	link: {
@@ -18,9 +18,22 @@ const useStyles = makeStyles({
 		color: "#D5D5D5",
 		cursor: "pointer",
 		fontWeight: 500,
-		fontSize: "180px"
+		fontSize: "18px",
+		[theme.breakpoints.down("md")]: {
+			fontWeight: 300,
+			fontSize: "13px"
+		}
 	},
-});
+	followText: {
+		color: "#D5D5D5",
+		fontWeight: 500,
+		fontSize: "18px",
+		[theme.breakpoints.down("md")]: {
+			fontWeight: 300,
+			fontSize: "13px"
+		}
+	}
+}));
 
 const Footer = () => {
 
@@ -29,33 +42,33 @@ const Footer = () => {
 
 	return(
 		<footer style={{ width: "100%" }}>
-			<Stack className={classes.contentContainer} justifyContent="flex-start">
+			<Stack className={classes.contentContainer} justifyContent="flex-start" alignItems="center">
 
-				<Stack marginTop={{ md: "90px", sm: "30px", xs: "30px" }} spacing={9}>
+				<Stack  width="90%" marginTop={{ md: "90px", sm: "30px", xs: "30px" }}  alignItems="space-around" spacing={9}>
 					<Typography color="#D5D5D5" variant="h4" sx={{ display: { md: "initial", sm: "initial", xs: "none" } }}>TM Lucasfilm Ltd. All Rights Reserved.</Typography>
 
 					<Grid container md={12} justifyContent="space-around" alignItems="center">
 						<Grid container item md={8} sm={12} direction="row" justifyContent="space-around">
-							<Stack gap={3}>
-								<Typography className={classes.link}>Terms of Use</Typography>
-								<Typography className={classes.link}>Legal Notices</Typography>
-								<Typography className={classes.link}>Privacy Policy</Typography>
+							<Stack gap={1}>
+								<p className={classes.link}>Terms of Use</p>
+								<p className={classes.link}>Legal Notices</p>
+								<p className={classes.link}>Privacy Policy</p>
 							</Stack>
 
-							<Stack  gap={3}>
-								<Typography className={classes.link}>Your California Privacy Rights</Typography>
-								<Typography className={classes.link}>Star Wars at Disney Store</Typography>
-								<Typography className={classes.link}>Privacy Policy</Typography>
+							<Stack  gap={1}>
+								<p className={classes.link}>Your California Privacy Rights</p>
+								<p className={classes.link}>Star Wars at Disney Store</p>
+								<p className={classes.link}>Privacy Policy</p>
 							</Stack>
 						</Grid>
 
-						<Grid container item md={4} sm={12} alignItems="center">
-							<Stack flexDirection={{ md: "column", sm:"row", xs: "row" }} gap={2}>
-								<Typography color="#D5D5D5">FOLLOW STAR WARS</Typography>
+						<Grid container item md={4} sm={12} alignItems="center" marginTop={4}>
+							<Stack width="90%" flexDirection={{ md: "column", sm:"row", xs: "row" }}  alignItems="center" gap={2}>
+								<p color="#D5D5D5" className={classes.followText}>FOLLOW STAR WARS</p>
 								<Stack flexDirection="row" justifyContent="center" alignItems="center" gap={7}>
-									<Facebook />
-									<Twitter />
-									<img src={Instagram} alt="icon-instagram" />
+									<Facebook style={{ cursor: "pointer" }} />
+									<Twitter style={{ cursor: "pointer" }} />
+									<img src={Instagram} alt="icon-instagram" style={{ cursor: "pointer" }}/>
 								</Stack>
 							</Stack>
 						</Grid>
@@ -68,25 +81,3 @@ const Footer = () => {
 }
 
 export default Footer
-
-{/* <Stack width="100%" justifyContent="space-around" alignItems="center">
-	<Stack width="100%" flexDirection="row" bgcolor="red" justifyContent="space-around" alignItems="center">
-		<Stack gap={3}>
-			<Typography className={classes.link} variant="h6">Terms of Use</Typography>
-			<Typography className={classes.link} variant="h6">Legal Notices</Typography>
-			<Typography className={classes.link} variant="h6">Privacy Policy</Typography>
-		</Stack>
-
-		<Stack  gap={3}>
-			<Typography className={classes.link} variant="h6">Your California Privacy Rights</Typography>
-			<Typography className={classes.link} variant="h6">Star Wars at Disney Store</Typography>
-			<Typography className={classes.link} variant="h6">Privacy Policy</Typography>
-		</Stack>
-
-		<Stack>
-			<Typography className={classes.link} variant="h6">Terms of Use</Typography>
-			<Typography className={classes.link} variant="h6">Legal Notices</Typography>
-			<Typography className={classes.link} variant="h6">Privacy Policy</Typography>
-		</Stack>
-	</Stack>
-</Stack> */}
